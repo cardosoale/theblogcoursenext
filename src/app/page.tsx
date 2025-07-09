30c2218a2281cb8db838aa65f93547ccf2270eb4
Alexandre Cardoso <cardoal01@gmail.com>
2025-07-09 10:55:24 -0300

    feat: add PostImageCover component for improved post image rendering in HomePage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import { Container } from '@/components/Container';
 import { Header } from '@/components/Header';
+import { PostImageCover } from '@/components/PostCoverImage';
 import { PostHeading } from '@/components/PostHeading';
 import { PostsList } from '@/components/PostsLists';
 import { SpinLoader } from '@/components/SpinLoader';
@@ -13,16 +14,18 @@ export default function HomePage() {
       <Header />
 
       <section className='grid grid-cols-1 gap-8 mb-16 sm:grid-cols-2 group'>
-        <Link className='w-full h-full overflow-hidden rounded-xl' href='#'>
-          <Image
-            className='w-full h-full object-cover object-center group-hover:scale-105 transition'
-            src='/images/bryen_0.png'
-            width={1200}
-            height={720}
-            alt='Titulo do post'
-            priority
-          ></Image>
-        </Link>
+        <PostImageCover
+          linkProps={{
+            href: '#',
+          }}
+          imageProps={{
+            width: 1200,
+            height: 720,
+            src: '/images/bryen_9.png',
+            alt: 'alt da imagem',
+            priority: true,
+          }}
+        />
         <div className='flex flex-col gap-4 sm: justify-center'>
           <time
             className='text-slate-600 block text-sm/tight'
---
+++ src/components/PostCoverImage/index.tsx
@@ -0,0 +1,38 @@
+import Link from 'next/link';
+import Image from 'next/image';
+import clsx from 'clsx';
+
+type PostImageCoverProps = {
+  imageProps: React.ComponentProps<typeof Image>;
+  linkProps: React.ComponentProps<typeof Link>;
+};
+
+export function PostImageCover({ imageProps, linkProps }: PostImageCoverProps) {
+  return (
+    <Link
+      {...linkProps}
+      className={clsx(
+        'w-full',
+        'h-full',
+        'overflow-hidden',
+        'rounded-xl',
+        linkProps.className,
+      )}
+      href='#'
+    >
+      <Image
+        {...imageProps}
+        className={clsx(
+          'w-full',
+          'h-full',
+          'object-cover',
+          'object-center',
+          'group-hover:scale-105',
+          'transition',
+          imageProps.className,
+        )}
+        alt={imageProps.alt}
+      ></Image>
+    </Link>
+  );
+}

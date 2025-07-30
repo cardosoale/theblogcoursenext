27df99d7d6fe15b4834df630ba0f6107f7054191
Alexandre Cardoso <cardoal01@gmail.com>
2025-07-30 10:46:54 -0300

    feat: add confirmation modal for post deletion in PostsListAdmin component

--- src/components/PostsListAdmin/index.tsx
+++ src/components/PostsListAdmin/index.tsx
@@ -30,6 +30,50 @@ export default async function PostsListAdmin() {
           </div>
         );
       })}
+
+      <div
+        className={clsx(
+          'fixed z-50 inset-0 bg-black/50 backdrop-blur-xs',
+          'flex items-center justify-center',
+        )}
+      >
+        <div
+          className={clsx(
+            'bg-slate-100 p-6 rounded-lg max-w-2xl mx-6',
+            'flex flex-col gap-6',
+            'shadow-lg shadow-black/30 text-center',
+          )}
+        >
+          <h3 className='text-xl font-extrabold'>Titulo</h3>
+          <p className='text-justify mx-6'>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Velit
+            pariatur commodi excepturi atque laboriosam necessitatibus minus
+            aliquid blanditiis quisquam, officia obcaecati, sunt aut perferendis
+            inventore repellendus provident repudiandae eligendi vitae!
+          </p>
+          <div className='flex items-center justify-around'>
+            <button
+              className={clsx(
+                'bg-slate-300 hover:bg-slate-400 transition text-slate-900',
+                'flex items-center justify-center',
+                'py-2 px-4 rounded-lg cursor-pointer',
+              )}
+              autoFocus
+            >
+              Cancelar
+            </button>
+            <button
+              className={clsx(
+                'bg-red-500 hover:bg-red-600 transition text-red-50',
+                'flex items-center justify-center',
+                'py-2 px-4 rounded-lg cursor-pointer',
+              )}
+            >
+              Deletar
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }

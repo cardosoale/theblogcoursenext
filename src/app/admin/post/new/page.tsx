af2b3e318ed941daa3d8bf0f771eafcd07de2489
Alexandre Cardoso <cardoal01@gmail.com>
2025-08-03 12:14:45 -0300

    feat: create reusable Button component with variant and size props

--- src/app/admin/post/new/page.tsx
+++ src/app/admin/post/new/page.tsx
@@ -1,5 +1,19 @@
-export const dynamic = 'force-static';
+import { Button } from '@/components/Button';
+
+export const dynamic = 'force-dynamic';
 
 export default async function AdminPostNewPage() {
-  return <div className='py-16 text-6xl'>AdminPostNewPage</div>;
+  return (
+    <div className='py-16 flex gap-4 flex-wrap'>
+      <Button variant='default' size='sm' type='submit'>
+        Confirm
+      </Button>
+      <Button variant='ghost' size='md' type='submit'>
+        Confirm
+      </Button>
+      <Button variant='danger' size='lg' type='submit'>
+        Confirm
+      </Button>
+    </div>
+  );
 }
---
+++ src/components/Button/index.tsx
@@ -0,0 +1,29 @@
+import clsx from 'clsx';
+
+type ButtonProps = {
+  variant?: ButtonVariants;
+  size?: ButtonSizes;
+} & React.ComponentProps<'button'>;
+type ButtonVariants = 'default' | 'ghost' | 'danger';
+type ButtonSizes = 'sm' | 'md' | 'lg';
+
+export function Button({
+  variant = 'default',
+  size = 'md',
+  ...props
+}: ButtonProps) {
+  const buttonVariants: Record<ButtonVariants, string> = {
+    default: clsx('bg-blue-600 text-blue-100'),
+    ghost: clsx('bg-slate-200 text-slate-900'),
+    danger: clsx('bg-red-600 text-red-100'),
+  };
+
+  const buttonSizes: Record<ButtonSizes, string> = {
+    sm: clsx(''),
+    md: clsx(''),
+    lg: clsx(''),
+  };
+  const buttonClasses = clsx(buttonVariants[variant], buttonSizes[size]);
+
+  return <button className={buttonClasses} {...props} />;
+}

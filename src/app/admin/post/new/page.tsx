51263802bb881878d670d365d9e2c977beef7f4b
Alexandre Cardoso <cardoal01@gmail.com>
2025-08-05 15:14:40 -0300

    feat: add InputText component with label support and styling

--- src/app/admin/post/new/page.tsx
+++ src/app/admin/post/new/page.tsx
@@ -1,19 +1,5 @@
-import { Button } from '@/components/Button';
-
 export const dynamic = 'force-dynamic';
 
 export default async function AdminPostNewPage() {
-  return (
-    <div className='py-16 flex gap-4 flex-wrap'>
-      <Button variant='default' size='sm' type='submit'>
-        Confirm
-      </Button>
-      <Button variant='ghost' size='md' type='submit'>
-        Confirm
-      </Button>
-      <Button variant='danger' size='lg' type='submit'>
-        Confirm
-      </Button>
-    </div>
-  );
+  return <p>new page</p>;
 }

5fb3b6982b80e19085c0a74fe69ad52346fa8c43
Alexandre Cardoso <cardoal01@gmail.com>
2025-08-06 09:43:46 -0300

    feat: add Markdown editor and input components for post creation
    - Added InputCheckbox component for checkbox inputs.
    - Introduced MarkdownEditor component for rich text editing.
    - Created ManagePostForm component to handle post creation with various input types.
    - Updated AdminPostNewPage to include ManagePostForm for creating new posts.
    - Added @uiw/react-md-editor dependency for Markdown editing functionality.

--- src/app/admin/post/new/page.tsx
+++ src/app/admin/post/new/page.tsx
@@ -1,5 +1,12 @@
+import { ManagePostForm } from '@/components/admin/ManagePostForm';
+
 export const dynamic = 'force-dynamic';
 
 export default async function AdminPostNewPage() {
-  return <p>new page</p>;
+  return (
+    <>
+      <h1>Criar Post</h1>
+      <ManagePostForm />
+    </>
+  );
 }
